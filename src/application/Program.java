fd0c4c567ed8b82e5001bffd7f8e13b30ab92a8e
CR7sewey <miguelmotacarvalho@gmail.com>
2023-10-24 10:24:26 +0100

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
@@ -28,7 +31,10 @@ public class Program {
 		 */
 		
 		Department obj = new Department(1,"Books");
+		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 		
 		
 		
