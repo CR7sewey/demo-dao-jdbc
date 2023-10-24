f6221d6d8a866215bc6d5849cc456f83292151aa
CR7sewey <miguelmotacarvalho@gmail.com>
2023-10-24 10:18:50 +0100

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Referências:
+		 * https://www.devmedia.com.br/dao-pattern-persistencia-de-dados-utilizando-o-
+		 * padrao-dao/30999
+		 * https://www.oracle.com/technetwork/java/dataaccessobject-138824.html Ideia
+		 * geral do padrão DAO:  Para cada entidade, haverá um objeto responsável por
+		 * fazer acesso a dados relacionado a esta entidade. Por exemplo: o Cliente:
+		 * ClienteDao o Produto: ProdutoDao o Pedido: PedidoDao  Cada DAO será definido
+		 * por uma interface.  A injeção de dependência pode ser feita por meio do
+		 * padrão de projeto Factory
+		 * 
+		 * ~
+		 * ClienteDao é o objeto reponsavel por fazer o acesso de dados a clientes
+		 * etc
+		 * 
+		 * Cada Dao é representado por uma interface, visto que o acesso a dados pode mudar
+		 * Hoje SQl e amanha outro banco de dados oracle, hoje DAO ou jdbc ou jpa e amanha migrar para outra tech (tipo orn)
+		 * 
+		 */
+		
+		Department obj = new Department(1,"Books");
+		System.out.println(obj);
+		
+		
+		
+
+	}
+
+}
---
+++ src/entities/Seller.java
@@ -0,0 +1,15 @@
+package entities;
+
+
+
+public class Seller {
+	
+	private Integer id;
+	private String name;
+	private String email;
+	//private Date birthDate;
+	private Double baseSalary;
+	
+	private Department dep;
+
+}
